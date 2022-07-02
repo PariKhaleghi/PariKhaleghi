a162b564042cdc3256070aa57b62e22d96f2befb
Mehranalam <mehranalambeigi@gmail.com>
2022-07-02 10:00:21 +0430

    Update

--- FOOTER.md
+++ FOOTER.md
@@ -2,5 +2,5 @@
 <div align="center">
 README.md last auto generated {timestamp}
 <br>
-<a href="https://parikhaleghi.ir" target="_blank">The mechanism of this profile.</a>
+<a href="https://parikhaleghi.ir" target="_blank">The mechanism of this profile</a>
 </div>
--- build.py
+++ build.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 import datetime
 import pytz
-
 import feedparser
 
 
 def update_footer():
     timestamp = datetime.datetime.now(
-        pytz.timezone("Europe/Madrid")).strftime("%c")
+        pytz.timezone("Asia/Tehran")).strftime("%c")
     footer = Path('./FOOTER.md').read_text()
     return footer.format(timestamp=timestamp)
 
@@ -16,15 +15,13 @@ def update_readme_medium_posts(Pari_post_path, readme_base, join_on):
     d = feedparser.parse(Pari_post_path)
     posts = []
     for item in d.entries:
-        posts.append("|")
         if item.get('tags'):
             posts.append(f"- [{item['title']}]({item['link']})")
-        posts.append("|")
     posts_joined = '\n'.join(posts)
     return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
 
 
-rss_title = "### Parisa's latest posts on [parikhaleghi.ir](https://parikhaleghi.ir)"
+rss_title = "### آخرین پست های پریسا در [parikhaleghi.ir](https://parikhaleghi.ir)"
 readme = Path('./README.md').read_text()
 updated_readme = update_readme_medium_posts(
     "https://parikhaleghi.ir/feed/", readme, rss_title)

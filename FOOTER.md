6c2472bfe118f6f007bf13df668173a93c28d46c
Mehranalam <mehranalambeigi@gmail.com>
2022-07-02 09:49:31 +0430

    Update

--- FOOTER.md
+++ FOOTER.md
@@ -1,6 +1,7 @@
+</td></tr></table>
 <hr>
 <div align="center">
 README.md last auto generated {timestamp}
 <br>
-<a href="https://medium.com/@dylanroy" target="_blank">Learn to automate you own README here!</a>
+<a href="https://parikhaleghi.ir" target="_blank">The mechanism of this profile.</a>
 </div>
--- build.py
+++ build.py
@@ -15,18 +15,17 @@ def update_footer():
 def update_readme_medium_posts(Pari_post_path, readme_base, join_on):
     d = feedparser.parse(Pari_post_path)
     posts = []
+    posts.append("""<table><tr><td valign="top" width="33%">""")
     for item in d.entries:
-        posts.append("""<td valign="top" width="33%">""")
         if item.get('tags'):
             posts.append(f"- [{item['title']}]({item['link']})")
-        posts.append("""<td/>""")
     posts_joined = '\n'.join(posts)
     return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
 
 
-rss_title = "### آخرین پست های من در "
+rss_title = "### Parisa's latest posts on [parikhaleghi.ir](https://parikhaleghi.ir)"
 readme = Path('./README.md').read_text()
 updated_readme = update_readme_medium_posts(
-    "parikhaleghi.ir", readme, rss_title)
+    "https://parikhaleghi.ir/feed/", readme, rss_title)
 with open('./README.md', "w+") as f:
     f.write(updated_readme + update_footer())

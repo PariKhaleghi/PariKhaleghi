afb5bdf31e9d08611a7939e4694fa42774c8526e
Mehranalam <mehranalambeigi@gmail.com>
2022-07-17 02:53:24 +0430

    Add CppRef

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import datetime
+from pyparsing import empty
 import pytz
+from requests import post
 import feedparser
 
 
@@ -11,19 +13,44 @@ def update_footer():
     return footer.format(timestamp=timestamp)
 
 
-def update_readme_medium_posts(Pari_post_path, readme_base, join_on):
+def update_readme_Pari_posts(Pari_post_path, readme_base, join_on):
     d = feedparser.parse(Pari_post_path)
     posts = []
     for item in d.entries:
         if item.get('tags'):
-            posts.append(f"- [{item['title']}]({item['link']})")
+            posts.append(
+                f"- [{item['title']}]({item['link']}) - {item['updated']}")
     posts_joined = '\n'.join(posts)
-    return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
+    return readme_base[:readme_base.find(rss_title_blog)] + f"{join_on}\n{posts_joined}"
 
 
-rss_title = "### آخرین پست های پریسا در [parikhaleghi.ir](https://parikhaleghi.ir)"
+def get_post_from_cpp_reference(Pari_cpp_post, readme_base, join_on):
+    d = feedparser.parse(Pari_cpp_post)
+    posts = []
+    for item in d.entries:
+        if item.get('title'):
+            posts.append(
+                f"- [{item['title']}]({item['link']}) - {item['updated']}")
+
+    for i in range(0, len(posts)):
+        for j in range(i+1, len(posts)):
+            if(j >= len(posts)):
+                break
+            if(posts[i] == posts[j]):
+                objectt = str(posts[i])
+                posts.remove(objectt)
+
+    print(posts)
+    posts_joined = '\n'.join(posts)
+    return readme_base[:readme_base.find(rss_title_cppRef)] + f"{join_on}\n{posts_joined}"
+
+
+rss_title_blog = "### آخرین پست های پریسا در [parikhaleghi.ir](https://parikhaleghi.ir)"
+rss_title_cppRef = "### آخرین پست های پریسا در [cppreference.com ](https://cppreference.com)"
 readme = Path('./README.md').read_text()
-updated_readme = update_readme_medium_posts(
-    "https://parikhaleghi.ir/feed/", readme, rss_title)
+updated_readme_blog = update_readme_Pari_posts(
+    "https://parikhaleghi.ir/feed/", readme, rss_title_blog)
+add_cpp_reference = get_post_from_cpp_reference(
+    "https://en.cppreference.com/mwiki/api.php?action=feedcontributions&user=Parisakhaleghi&feedformat=rss", readme, rss_title_cppRef)
 with open('./README.md', "w+") as f:
-    f.write(updated_readme + update_footer())
+    f.write(add_cpp_reference + "<br> <br>" +updated_readme_blog + update_footer())
